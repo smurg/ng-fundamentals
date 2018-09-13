6ff1b783336a73a49d214a1d2ee8c446728f4580
smurg <smurgolo@growthaccelerationpartners.com>
2018-09-13 10:34:47 -0300

    added routes, event service to fetch events and update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/events.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
+/*
+we have to register all of our components within the modules
+declarations: when you wanna add a component, pipe or directive we must declare it inside that array.
+            First importing it and then adding it to the array, like AppComponent.
+            If the component is not declared there, you will have an error when using it!!!!
+
+imports:    Used to import other modules. BrowserModule will give access to a lot of built in angular features.
+
+Providers:  is to declare the services that this module will use.
+
+bootstrap:  declares which component will be the initial to load.
+*/
 export class AppModule { }
---
+++ src/app/routes.ts
@@ -0,0 +1,11 @@
+import { EventsListComponent } from './events/events-list.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { Routes } from '@angular/router';
+/* Routes has all the types and methods to create Route objects */
+export const appRoutes: Routes = [
+  { path: 'events', component: EventsListComponent },
+  { path: 'events/:id', component: EventDetailsComponent },
+  { path: '', redirectTo: '/events', pathMatch: 'full' }
+];
+/* it's an array of Route objects:
+if the url match '/events' show the component EventsListComponent */
